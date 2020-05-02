204a8a3fd4989ed07f6effb6290f21ef72d3ff46
Carlos Del Real <carloshdelreal@gmail.com>
2020-05-01 19:33:47 -0500

    finished exercise

---
+++ main.js
@@ -0,0 +1,23 @@
+Vue.component('coupon', {
+  template: '<input placeholder="enter your coupon" @blur="onCouponApplied">',
+
+  methods: {
+    onCouponApplied() {
+      this.$emit('applied')
+    }
+  }
+})
+
+new Vue({
+  el: '#root',
+
+  data: {
+    couponApplied: false
+  },
+
+  methods: {
+    onCoupon() {
+      this.couponApplied = true;
+    }
+  }
+})
